6f84fb83e163c6c60644aaff8a441c7831369c35
Chris Bloom <bloomcb@gmail.com>
2012-03-10 13:34:52 -0800

    parralax testing

--- js/script.js
+++ js/script.js
@@ -2,6 +2,24 @@
 
 */
 
+$(document).ready(function() {
+  var scrollorama = $.scrollorama({
+    blocks:'.scrollblock',
+    enablePin:false
+  });
+
+  scrollorama.onBlockChange(function() {
+    var i = scrollorama.blockIndex;
+    $('#console')
+      .css('display','block')
+      .text('onBlockChange | blockIndex:'+i+' | current block: '+scrollorama.settings.blocks.eq(i).attr('id'));
+  });
+
+  scrollorama.animate('#biker',{ duration: 50, property:'left', start:-100, end:0 });
+
+
+  console.log('hello');
+});
 
 
 
