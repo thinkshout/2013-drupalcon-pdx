25a867185529d0158d9c06131e9b31eb5472daf7
Chris Bloom <bloomcb@gmail.com>
2012-03-15 19:40:21 -0700

    setting user scalable null in meta, removing conole logging for scrollorama stuff

--- js/script.js
+++ js/script.js
@@ -15,10 +15,7 @@ function scrollUpInDis(selector){
   if(selector !== 'goaway' && firedScroll === false){
     console.log('running it all again');
     scrollorama.onBlockChange(function() {
-      var i = scrollorama.blockIndex;
-      $('#console')
-        .css('display','block')
-        .text('onBlockChange | blockIndex:'+i+' | current block: '+scrollorama.settings.blocks.eq(i).attr('id'));
+
     });
     scrollorama.animate('#biker-1',
       {
